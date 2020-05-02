65513580bcc7ec0a9a404bba79ab48e1f58725b7
Lucas Thomaz Ribeiro <lucasthomazr@hotmail.com>
2020-05-02 16:38:42 -0300

    Patch Route Done

--- backend/src/repositories/goalRepository.ts
+++ backend/src/repositories/goalRepository.ts
@@ -11,9 +11,17 @@ interface Difference {
 class GoalsRepository extends Repository<Goal> {
   public getDifference(totalvalue: number, sparedvalue: number): Difference {
     const dif = totalvalue - sparedvalue;
-    const percent = ((sparedvalue / totalvalue) * 100).toFixed(2);
+    const percent = ((sparedvalue / totalvalue) * 100).toFixed(1);
 
     return { dif, percent };
+  }
+
+  public async findOneGoal(id: string): Promise<Goal> {
+    const findGoal = await this.findOneOrFail({
+      where: { id },
+    });
+
+    return findGoal;
   }
 }
 
--- backend/src/routes/goal.routes.ts
+++ backend/src/routes/goal.routes.ts
@@ -3,8 +3,30 @@ import { parseISO } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import CreateGoalService from '../services/CreateGoalService';
 import GoalsRepository from '../repositories/goalRepository';
+import UpdateGoalService from '../services/UpdateGoalService';
 
 const goalsRouter = Router();
+
+goalsRouter.get('/', async (request, response) => {
+  const goalsRepository = getCustomRepository(GoalsRepository);
+
+  // Para listar somente as metas do usuário que está pedindo elas
+  const goals = await goalsRepository.find({
+    where: { userid: request.headers.authorization },
+  });
+
+  // Encontra a diferença e o percentual que falta de cada meta
+  const goalobject = goals.map(goal => {
+    const intel = goalsRepository.getDifference(
+      goal.totalvalue,
+      goal.sparedvalue,
+    );
+
+    return { goal, intel };
+  });
+
+  return response.json(goalobject);
+});
 
 goalsRouter.post('/', async (request, response) => {
   try {
@@ -29,6 +51,21 @@ goalsRouter.post('/', async (request, response) => {
     const difference = goalsrepository.getDifference(totalvalue, sparedvalue);
 
     return response.json({ goal, difference });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+goalsRouter.patch('/:id', async (request, response) => {
+  try {
+    const { sparedvalue } = request.body;
+    const { id } = request.params;
+
+    const updateGoal = new UpdateGoalService();
+
+    await updateGoal.execute(id, sparedvalue);
+
+    return response.status(204).json({ message: 'Altered' });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
