f393a07ab1d51f00942708b4e5b131a0269e9edc
Lucas Thomaz Ribeiro <lucasthomazr@hotmail.com>
2020-05-03 17:12:31 -0300

    Register Page

--- mobile/src/pages/register/index.js
+++ mobile/src/pages/register/index.js
@@ -1,39 +1,69 @@
 import React from 'react';
-import { View, Text, StatusBar, Image, TextInput } from 'react-native';
+import { View, Text, StatusBar, Image, TextInput, ScrollView, TouchableOpacity, CheckBox } from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view';
+import { useNavigation } from '@react-navigation/native';
 import styles from './styles';
-import backArrow from '../../assets/arrowback.svg'
+import backArrow from '../../assets/arrowback1.png';
+import pin from '../../assets/bolinha.png';
 
 export default function Register() {
+
+  const navigation = useNavigation();
+
+  function navigateBack() {
+    navigation.goBack();
+  }
+
   return (
     <>
       <StatusBar barStyle='light-content' backgroundColor='#000' />
       <SafeAreaView style={styles.container}>
-        <View style={styles.back}>
+        <TouchableOpacity style={styles.back} activeOpacity={0.6} onPress={navigateBack}>
           <Image source={backArrow} style={styles.backlogo} />
           <Text style={styles.backText}>
             Voltar
         </Text>
-        </View>
+        </TouchableOpacity>
         <View style={styles.line}></View>
-        <View style={styles.body}>
+        <ScrollView style={styles.body}>
           <Text style={styles.text}>Olá, seja bem vindo(a) ao Flourish.</Text>
           <View style={styles.inputComponent}>
             <Text style={styles.text}>Como posso te chamar?</Text>
-            <TextInput placeholder={'Digite aqui'} keyboardType={'default'} ></TextInput>
+            <TextInput style={styles.inputText} placeholder={'Digite aqui'} keyboardType={'default'} ></TextInput>
           </View>
           <View style={styles.inputComponent}>
             <Text style={styles.text}>Qual o seu e-mail?</Text>
-            <TextInput placeholder={'flourish@flourish.com'} keyboardType={'email-address'}></TextInput>
+            <TextInput style={styles.inputText} placeholder={'flourish@flourish.com'} keyboardType={'email-address'}></TextInput>
           </View>
           <View style={styles.inputComponent}>
             <Text style={styles.text}>Senha</Text>
-            <TextInput placeholder={'**********'} keyboardType={'default'} secureTextEntry={true} ></TextInput>
-            <Text style={styles.passwordsubtitle}>Precisa ter ao menos 8 caracters</Text>
-            <Text style={styles.passwordsubtitle}>Precisa ter ao menos 1 número</Text>
-            <Text style={styles.passwordsubtitle}>Precisa ter ao menos 1 letra maiúscula</Text>
+            <TextInput passwordRules={'required: upper, required: lower, required: digit minlength: 8'} style={styles.inputText} placeholder={'Digite aqui'} keyboardType={'default'} secureTextEntry={true} ></TextInput>
+            <View style={styles.unitsubtitle}>
+              <Image source={pin} style={styles.bullet} />
+              <Text style={styles.passwordsubtitle}>Precisa ter ao menos 8 caracters</Text>
+            </View>
+            <View style={styles.unitsubtitle}>
+              <Image source={pin} style={styles.bullet} />
+              <Text style={styles.passwordsubtitle}>Precisa ter ao menos 1 número</Text>
+            </View>
+            <View style={styles.unitsubtitle}>
+              <Image source={pin} style={styles.bullet} />
+              <Text style={styles.passwordsubtitle}>Precisa ter ao menos 1 letra maiúscula</Text>
+            </View>
+            <View style={styles.checked}>
+              <CheckBox  />
+              <Text style={styles.text}>Aceito os TERMOS & CONDIÇÕES</Text>
+            </View>
           </View>
-        </View>
+          <View style={styles.bottomContainer}>
+          <TouchableOpacity style={styles.button} activeOpacity={0.6}>
+            <Text style={styles.textbutton}>Concluir e Cadastrar Cartão</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button2} activeOpacity={0.6}>
+            <Text style={styles.textbutton2}>Adicionar o cartão depois</Text>
+          </TouchableOpacity>
+          </View>
+        </ScrollView>
       </SafeAreaView>
     </>
   );
--- mobile/src/pages/register/styles.js
+++ mobile/src/pages/register/styles.js
@@ -6,17 +6,153 @@ const styles = StyleSheet.create({
     backgroundColor: '#F8F8F2'
   },
 
-  header: {
+  back: {
+    flex: 1,
+    maxWidth: 100,
+    maxHeight: 40,
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
+    alignContent: 'center',
     alignItems: 'center',
-    marginLeft: 5,
+    marginTop: 10,
+    marginLeft: 10,
   },
 
-  headerText: {
-    fontSize: 25,
-    color: '#000',
+  backlogo: {
+    resizeMode: 'contain',
+    aspectRatio: 0.05,
   },
+
+  backText: {
+    fontFamily: 'SF',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 16,
+    lineHeight: 19,
+    color: '#535353',
+    marginLeft: 6,
+    flex: 1,
+  },
+
+  line: {
+    backgroundColor: '#347178',
+    maxHeight: 2,
+    flex: 1,
+    marginRight: 6,
+    marginLeft: 6,
+    marginTop: 10,
+  },
+
+  body: {
+    flex: 1,
+    marginTop: 5,
+    marginLeft: 10,
+    paddingTop: 20,
+  },
+
+  text: {
+    fontSize: 16,
+    fontFamily: 'SF',
+    fontWeight: 'normal',
+    lineHeight: 21,
+    color: '#535353',
+  },
+
+  inputComponent: {
+    marginTop: 25,
+  },
+
+  inputText: {
+    flex: 1,
+    maxHeight: 56,
+    marginRight: 10,
+    paddingLeft: 10,
+    marginTop: 16 ,
+    borderRadius: 4,
+    borderStyle: 'solid',
+    borderColor: '#BEBEBE',
+    borderWidth: 1,
+  },
+
+  unitsubtitle: {
+    flex: 1,
+    flexDirection: 'row',
+    marginTop: 8,
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+
+  bullet: {
+    resizeMode: 'contain',
+    aspectRatio: 1.5,
+  },
+
+  passwordsubtitle: {
+    fontFamily: 'SF',
+    fontSize: 14,
+    lineHeight: 17,
+    color: '#7B7B7B',
+    marginLeft: 5
+  },
+
+  checked: {
+    marginTop: 24,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+
+  button: {
+    flex: 1,
+    height: 50,
+    marginLeft: 15,
+    marginRight: 15,
+    marginTop: 24,
+    backgroundColor: '#347178',
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  button2: {
+    flex: 1,
+    height: 50,
+    marginLeft: 15,
+    marginTop: 20,
+    marginRight: 15,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+  },
+
+  textbutton2:{
+    fontFamily: 'SF',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 16,
+    lineHeight: 19,
+    color: '#347178'
+  },
+
+  textbutton: {
+    fontFamily: 'SF',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 16,
+    lineHeight: 19,
+    color: '#FFFFFF'
+  },
+
+  bottomContainer: {
+    flex: 1,
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    alignContent: 'flex-end',
+  },
+
+
 });
 
 export default styles;
